7b068ba4541f0014c5eb145a9d55ed092d946a81
Nicolò <nicandman@gmail.com>
2025-03-12 18:49:19 +0100

    Update fas

--- src/app/libs/vaults.ts
+++ src/app/libs/vaults.ts
@@ -39,7 +39,7 @@ export const VAULTS: Vault[] = [
     code: 'FAS_USDC',
     tvl: '$11m',
     yieldType: '7-day Yield',
-    yield: '13.45%',
+    yield: '6.45%',
     redemptions: 'Weekly',
     app: '//app.pareto.credit',
     type: 'credit',
